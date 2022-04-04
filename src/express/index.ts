ef945753ddddf659f11341cd422c5d3d6db01a44
Ernest Irabor <irabeny89@gmail.com>
2022-04-04 22:19:28 +0100

    update server

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,25 +1,30 @@
 import express from "express";
+import authRouter from "./authRouters";
+import errorHandler from "./errorHandler";
 
 // instantiate express server
 const server = express();
 
 server.disable("x-powered-by");
 
-// allow json from client enabling req.body
+// allow json from client; enabling req.body
 server.use(express.json());
 
-// headers middleware; cors- allow all origin
-server.all("/", (_, res, next) => {
+// headers middleware;
+server.use((_, res, next) => {
+  // CORS - allow all origin
   res.setHeader("Access-Control-Allow-Origin", "*");
+  // allowed HTTP methods
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   next();
 });
 
 // index route
-server.get("/", (_, res) => {
-  res.status(200).send("api ok");
-});
-// TODO
-// accounts router
-server.use("/api", () => {});
+server.get("/", (_, res) => res.status(200).send("api ok"));
+
+// auth route
+server.use("/api", authRouter);
+// error handler
+server.use(errorHandler);
 
 export default server;
