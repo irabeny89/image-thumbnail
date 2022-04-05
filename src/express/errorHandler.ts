c2d327a061d6b7e6f8dce6c09f5482ac4112c046
Ernest Irabor <irabeny89@gmail.com>
2022-04-05 06:32:06 +0100

    refactor

--- src/express/errorHandler.ts
+++ src/express/errorHandler.ts
@@ -2,7 +2,6 @@ import { ErrorRequestHandler } from "express";
 import config from "../../config";
 
 const errorHandler: ErrorRequestHandler = (err, _, res) => {
-  console.error(err)
   res.status(500).send(config.generalErrorMessage)
 }
 
