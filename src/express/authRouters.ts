d9cc470eb1fdff75184008171bb4c5427758b8e2
Ernest Irabor <irabeny89@gmail.com>
2022-04-05 05:49:37 +0100

    rename file name

--- src/express/authRouters.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { loginHandler } from "./authHandlers";
-
-const authRouter = Router();
-
-authRouter.route("/login").post(loginHandler);
-
-export default authRouter;
